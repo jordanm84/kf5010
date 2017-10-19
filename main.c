8f32171b14a98942641fe4d7bf233b2c49c4e073
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-19 17:31:39 +0100

    Minor tidying up

--- main.c
+++ main.c
@@ -11,7 +11,13 @@
 #include "console.h"
 #include "tunnel_controller.h"
 
-#define N_VEHICLES 10
+enum {
+    N_VEHICLES = 20,         /* number of vehicles in the system */
+    APPROACH_LB = 1000000,   /* lower bound on time of approach to tunnel */
+    APPROACH_UB = 3000000,   /* upper bound on time of approach to tunnel */
+    IN_TUNNEL_LB = 500000,   /* lower bound on time travelling in tunnel */
+    IN_TUNNEL_UB = 1500000,  /* upper bound on time travelling in tunnel */
+};
 
 static void *vehicle_thr(void *arg);
 unsigned int scale_random(double r, int l, int u);
@@ -20,34 +26,35 @@ int main (void) {
     int rc;
     unsigned long i;
     pthread_t thread[N_VEHICLES];
-    
+
     console_init();
+    lcd_write_at(0, 0, "THE OTHER TYNE TUNNEL");
+    lcd_write_at(1, 0, "---------------------");
+
     for (i = 0; i < N_VEHICLES; i += 1) {
         rc = pthread_create(&thread[i], NULL, vehicle_thr, (void *)i);
         assert(rc == 0);
     }
-    for (i = 0; i < N_VEHICLES; i += 1) {
-        rc = pthread_join(thread[i], NULL);
-        assert(rc == 0);
-    }
-    
+
     while (true) {
         /* skip */
     }
     exit(0);
 }
 
+/**
+ * @brief Thread to simulate the behaviour of a vehicle
+ * @param arg the id of the thread
+ */
 static void *vehicle_thr(void *arg) {
     double r;
     unsigned long id = (unsigned long)arg;
-    unsigned long i;
     struct drand48_data rand_state;
     int rc;
     direction_t dir;
-    unsigned int delay;
 
     /* initialise random seed */
-    rc = srand48_r((long)id * 10 + time(NULL), &rand_state); 
+    rc = srand48_r((long)id * 10 + time(NULL), &rand_state);
     assert(rc == 0);
 
     while (true) {
@@ -63,7 +70,7 @@ static void *vehicle_thr(void *arg) {
         /* approach tunnel */
         rc = drand48_r(&rand_state, &r);
         assert(rc == 0);
-        usleep(scale_random(r, 1000000, 3000000));
+        usleep(scale_random(r, APPROACH_LB, APPROACH_UB));
 
         /* enter tunnel */
         enter_tunnel(dir);
@@ -71,13 +78,19 @@ static void *vehicle_thr(void *arg) {
         /* travel through tunnel */
         rc = drand48_r(&rand_state, &r);
         assert(rc == 0);
-        usleep(scale_random(r, 500000, 1500000));
+        usleep(scale_random(r, IN_TUNNEL_LB, IN_TUNNEL_UB));
 
         /* exit tunnel */
-        exit_tunnel(dir);       
+        exit_tunnel(dir);
     }
 }
 
+/**
+ * @brief Scale r to an integer in the range [l,u)
+ * @param r a random double in the range [0, 1)
+ * @param l the lower bound of the range
+ * @param u the upper bound of the range
+ */
 unsigned int scale_random(double r, int l, int u) {
     assert((0 <= r) && (r < 1) && (0 <= l) && (l < u));
     return (unsigned int)(lround(l + ((u - l) * r)));
--- tunnel_controller.c
+++ tunnel_controller.c
@@ -3,23 +3,23 @@
 #include "console.h"
 
 static int n_in_tunnel[2];
-static unsigned long total_through[2];
+static unsigned long n_total[2];
 
 void enter_tunnel(direction_t dir) {
     n_in_tunnel[dir] += 1;
 
-    lcd_write_at(dir, 0, "In tunnel (%s) : %d",
+    lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
-    /*assert(n_in_tunnel[opposite(dir)] == 0);*/
+    assert(n_in_tunnel[opposite(dir)] == 0);
 }
 
 void exit_tunnel(direction_t dir) {
     n_in_tunnel[dir] -= 1;
-    lcd_write_at(dir, 0, "In tunnel (%s) : %d",
+    lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
-    total_through[dir] += 1;
-    lcd_write_at(2+dir, 0, "Total     (%s) : %d",
-            (dir == NORTH) ? "NORTH" : "SOUTH", total_through[dir]);
+    n_total[dir] += 1;
+    lcd_write_at(6+dir, 0, "Total     (%s) : %d",
+            (dir == NORTH) ? "NORTH" : "SOUTH", n_total[dir]);
 }
 
 direction_t opposite(direction_t dir) {
