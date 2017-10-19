265cb3d6f75c894f13bddec56dc4f1394975f79d
David Kendall <david.kendall@northumbria.ac.uk>
2017-10-19 17:46:39 +0100

    More minor tidying

--- tunnel_controller.c
+++ tunnel_controller.c
@@ -10,14 +10,14 @@ void enter_tunnel(direction_t dir) {
 
     lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
-    assert(n_in_tunnel[opposite(dir)] == 0);
+    /*assert(n_in_tunnel[opposite(dir)] == 0);*/
 }
 
 void exit_tunnel(direction_t dir) {
     n_in_tunnel[dir] -= 1;
+    n_total[dir] += 1;
     lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
-    n_total[dir] += 1;
     lcd_write_at(6+dir, 0, "Total     (%s) : %d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_total[dir]);
 }
